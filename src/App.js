394dfbe3841d0dc01a716c7baf9b903a9d35152f
shubhamupadhyay09 <shubham9092001@gmail.com>
2023-08-14 22:36:28 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideBar from './Component/SideBar/SideBar'
+
+
+const App = () => {
+  return (
+    <div>
+     <SideBar/>
+      
+    </div>
+  )
+}
+
+export default App
+
